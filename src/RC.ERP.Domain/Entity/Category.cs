08a3aa440d929258207fbfc17793d926b56900c9
Rafael Santos Cunha <rafaelsantoscunha02@gmail.com>
2024-07-02 21:28:23 -0300

    Creation of the domain project and creation of the Category entity.
    Criação do projeto de domínio e criação da entidade Categoria.

---
+++ src/RC.ERP.Domain/Entity/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace RC.ERP.Domain.Entity
+{
+    public class Category
+    {
+        public Category(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+    }
+}
