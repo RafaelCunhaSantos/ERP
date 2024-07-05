12e197f8bf97a9ed5ffef821057c18359a938f93
Rafael Santos Cunha <rafaelsantoscunha02@gmail.com>
2024-07-04 21:33:37 -0300

    Creating the category unit test.
    Criando o teste unitário da categoria.

--- tests/RC.ERP.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ tests/RC.ERP.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -1,6 +1,28 @@
-﻿namespace RC.ERP.UnitTests.Domain.Entity
+﻿using Xunit;
+using DomainEntity = RC.ERP.Domain.Entity;
+
+namespace RC.ERP.UnitTests.Domain.Entity
 {
     public class CategoryTest
     {
+
+        public void Instantiate()
+        {
+            //Arrange
+            var validData = new
+            {
+                Name = "Nome Categoria",
+                Description = "Descrição Categoria",
+            };
+
+            //Act
+            var catagoty = new DomainEntity.Category(validData.Name, validData.Description);
+
+            //Assert
+            Assert.NotNull(catagoty);
+            Assert.Equal(validData.Name, catagoty.Name);
+            Assert.Equal(validData.Description, catagoty.Description);
+        }
+
     }
 }
