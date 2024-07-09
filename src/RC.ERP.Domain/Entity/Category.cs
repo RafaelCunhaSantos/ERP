aeae08fd08190809737c883748379ce8ec523377
Rafael Santos Cunha <rafaelsantoscunha02@gmail.com>
2024-07-09 09:27:51 -0300

    Performing initial testing of the category domain.
    Realizando teste iniciais do domínio de categoria.

--- src/RC.ERP.Domain/Entity/Category.cs
+++ src/RC.ERP.Domain/Entity/Category.cs
@@ -2,13 +2,20 @@
 {
     public class Category
     {
-        public Category(string name, string description)
-        {
-            Name = name;
-            Description = description;
-        }
-
+        public Guid Id { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
+        public bool IsActive { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        public Category(string name, string description)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            IsActive = true;
+            CreatedAt = DateTime.Now;
+        }
+
     }
 }
--- tests/RC.ERP.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ tests/RC.ERP.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -1,11 +1,13 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 using DomainEntity = RC.ERP.Domain.Entity;
 
 namespace RC.ERP.UnitTests.Domain.Entity
 {
     public class CategoryTest
     {
-
+        [Fact(DisplayName = nameof(Instantiate))]
+        [Trait("Domain", "Category - Aggregates")]
         public void Instantiate()
         {
             //Arrange
@@ -16,12 +18,19 @@ namespace RC.ERP.UnitTests.Domain.Entity
             };
 
             //Act
-            var catagoty = new DomainEntity.Category(validData.Name, validData.Description);
+            var datetimeBefore = DateTime.Now;
+            var category = new DomainEntity.Category(validData.Name, validData.Description);
+            var datetimeAfter = DateTime.Now;
 
             //Assert
-            Assert.NotNull(catagoty);
-            Assert.Equal(validData.Name, catagoty.Name);
-            Assert.Equal(validData.Description, catagoty.Description);
+            Assert.NotNull(category);
+            Assert.Equal(validData.Name, category.Name);
+            Assert.Equal(validData.Description, category.Description);
+            Assert.NotEqual(default(Guid), category.Id);
+            Assert.NotEqual(default(DateTime), category.CreatedAt);
+            Assert.True(category.CreatedAt > datetimeBefore);
+            Assert.True(category.CreatedAt < datetimeAfter);
+            Assert.True(category.IsActive);
         }
 
     }
